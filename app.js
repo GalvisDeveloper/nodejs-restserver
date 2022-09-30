f7ed6866af512eacab2a441c88defb04a651e7ad
GalvisDeveloper <galvisdeveloper@gmail.com>
2022-09-30 12:32:28 -0500

    Config node modules

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo')
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo')
+});
+
+app.listen(port, () => {
+    console.log(`The app is runing on port ${port}`)
+});
