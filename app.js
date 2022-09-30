58c6ed524246dab85ad09d0be2667c1cd36da074
GalvisDeveloper <galvisdeveloper@gmail.com>
2022-09-30 12:52:09 -0500

    Express basado en clases

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
+const Server = require('./models/server');
+
 require('dotenv').config();
-const express = require('express');
 
-const app = express();
+const server = new Server();
 
-const port = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-    res.send('Hola Mundo')
-});
-
-app.listen(port, () => {
-    console.log(`The app is runing on port ${port}`)
-});
+server.listen();
---
+++ models/server.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Middlewares 
+        this.middlewares();
+
+        // Routes
+        this.routes();
+    }
+
+    middlewares() {
+        // Public directory
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.get('/', (req, res) => {
+            res.send('Hola mundo')
+        });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`The app is running at ${this.port}`);
+        });
+    }
+
+}
+
+module.exports = Server;
