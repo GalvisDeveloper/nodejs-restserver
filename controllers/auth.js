04e46751f533cd7903cd205fb30e1a53f6c8f5c3
GalvisDeveloper <galvisdeveloper@gmail.com>
2022-10-03 22:26:33 -0500

    User login, only validating fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,46 @@
 const { response } = require('express');
 
+const bcryptjs = require('bcryptjs');
+
 const User = require('../models/user/user');
 
-const login = (req, res = response) => {
-    res.json({ algo: 'algo' });
+const login = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        // Verify if the email exist
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                msg: "User / Password is not correct",
+            })
+        }
+
+        // User has status active or true
+        if (!user.status) {
+            return res.status(400).json({
+                msg: "User is no longer active",
+            })
+        }
+
+        // Verify password
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: "Password is incorrect",
+            });
+        }
+
+        // Generate JWT
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error })
+    }
+
+    res.json({ email, password });
 }
 
 module.exports = {
