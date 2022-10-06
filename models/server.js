9f10ed758679014ec4faedcc2adf00a83fb7c61f
GalvisDeveloper <galvisdeveloper@gmail.com>
2022-10-05 20:07:48 -0500

    Sign In button by google implemented, thanks to their api key and api secret

--- models/server.js
+++ models/server.js
@@ -20,28 +20,30 @@ class Server {
 
         // Routes
         this.routes();
+
     }
 
-    async connectionDb(){
+    async connectionDb() {
         await dbConnection();
     }
-    
+
     middlewares() {
         // CORS 
         this.app.use(cors())
-        
+
         // Read and parse the body data
         this.app.use(express.json());
-        
+
         // Public directory
         this.app.use(express.static('public'));
+
     }
-    
+
     routes() {
         this.app.use(this.authPath, require('../routes/auth'))
         this.app.use(this.usersPath, require('../routes/user'))
     }
-    
+
     listen() {
         this.app.listen(this.port, () => {
             console.log(`The app is running at ${this.port}`);
