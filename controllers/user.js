a51f013304d04b447f470ae62e8fa8a93264ae16
GalvisDeveloper <galvisdeveloper@gmail.com>
2023-01-04 13:14:19 -0500

    pr test

--- controllers/user.js
+++ controllers/user.js
@@ -37,7 +37,7 @@ const getUserById = async (req, res = response) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({
-            msg: "Internal Server Error",
+            msg: "Internal Server Errorr",
         })
     }
 
